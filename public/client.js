69faaece679aa98b57d743ca9696c76b724555bc
VVNoodle <brickkace@gmail.com>
2017-11-24 13:17:00 -0800

    working on guess the drawing multiplayer browser game

---
+++ public/client.js
@@ -0,0 +1,74 @@
+// Wait for HTML page to load and ask the browser to tell when it's ready
+document.addEventListener("DOMContentLoaded", () => {
+  var mouse = {
+    click: false,
+    move: false,
+    pos: {
+      x: 0,
+      y: 0
+    },
+    pos_prev: false
+  };
+
+  // get canvas element
+  var canvas = document.getElementById("drawing");
+
+  // create a 2D drawing context to draw
+  var context = canvas.getContext("2d");
+
+  // window dimensions
+  var width = window.innerWidth;
+  var height = window.innerHeight;
+
+  // tell socket to connect to server
+  var socket = io.connect();
+
+  // sets the canvas width and height properties to the browser width and height
+  context.width = width;
+  context.height = height;
+
+  // mouse.click is true whenever we keep mouse button clicked
+  canvas.onmousedown = e => {
+    mouse.click = true;
+  };
+  canvas.onmouseup = e => {
+    mouse.click = false;
+  };
+
+  // mouse.pos.x and mouse.pos.y is updated whenever mouse is moved
+  canvas.onmousemove = e => {
+    mouse.pos.x = e.clientX / width;
+    mouse.pos.y = e.clientY / height;
+    // set mouse.move to true, so we can check if the mouse moved or not
+    mouse.move = true;
+  };
+
+  socket.on("draw_line", data => {
+    var line = data.newLine;
+
+    // start a new path
+    context.beginPath();
+    context.lineWidth = 2;
+
+    // move to the first point
+    context.moveTo(line[0].x * width, line[0].y * height);
+
+    // draw the line to the second received point
+    context.lineTo(line[1].x * width, line[1].y * height);
+
+    // actually draw the line
+    context.stroke();
+  });
+
+  function mainLoop() {
+    if (mouse.click && mouse.move && mouse.pos_prev) {
+      socket.emit("draw_line", {
+        line: [mouse.pos, mouse.pos_prev]
+      });
+      mouse.move = false;
+    }
+    mouse.pos_prev = { x: mouse.pos.x, y: mouse.pos.y };
+    setTimeout(mainLoop, 25);
+  }
+  mainLoop();
+});
---
+++ server.js
@@ -0,0 +1,34 @@
+var express = require("express");
+var app = express();
+var http = require("http");
+var socketIo = require("socket.io");
+
+var server = http.createServer(app);
+var io = socketIo.listen(server);
+app.use(express.static(__dirname + "/public"));
+server.listen(8080, () => {
+  console.log("Server running on localhost:8080");
+});
+
+// declare an array line_histry to keep strack of all lines ever drawn
+var line_history = [];
+
+// handler for new incoming connection
+// Whenever a new client connects, this
+// function is called and the socket of the new client is passed as an argument
+io.on("connection", socket => {
+  // send all the lines to a new client (just joined)
+  for (var i in line_history) {
+    socket.emit("draw_line", { line: line_history[i] });
+  }
+
+  //add a handler for our own message-type draw_line to the new client
+  //each time we recieve a line, we ad it to line_history and send it to
+  // all connected clients so they can update their canvases
+  socket.on("draw_line", data => {
+    // add received line to history
+    line_history.push(data.line);
+    // send line to all clients
+    io.emit("draw_line", { newLine: data.line });
+  });
+});
